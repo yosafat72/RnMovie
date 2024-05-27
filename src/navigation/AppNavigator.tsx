ae489dcacbf00d1fde815011c61aafd9de5f7f17
Yosafat Hermawan S <33362676+yosafat72@users.noreply.github.com>
2024-05-28 02:08:12 +0700

    add tabbarIcon

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faHeart, faHouse} from '@fortawesome/free-solid-svg-icons';
 
 // const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -11,8 +13,31 @@ const Tab = createBottomTabNavigator();
 
 function DashboardTab() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="DashboardHome" component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={{headerShown: false, tabBarActiveTintColor: '#e91e63'}}
+      initialRouteName="Home">
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color, size}) => (
+            <FontAwesomeIcon icon={faHouse} size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Favorite',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color, size}) => (
+            <FontAwesomeIcon icon={faHeart} size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -20,7 +45,7 @@ function DashboardTab() {
 function AppDrawer() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={DashboardTab} />
+      <Drawer.Screen name="Dashboard" component={DashboardTab} />
     </Drawer.Navigator>
   );
 }
